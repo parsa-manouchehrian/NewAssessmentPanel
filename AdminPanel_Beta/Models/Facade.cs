daa3168916745d9ce75fc099bf8402d8ae01143e
parsa-manouchehrian <>
2022-05-08 16:58:25 -0700

    Add the list of hints

--- AdminPanel_Beta/Models/Facade.cs
+++ AdminPanel_Beta/Models/Facade.cs
@@ -70,5 +70,21 @@ namespace AdminPanel_Beta.Models
                 .ToList();
         }
 
+        public AssessmentResultGroup GetResultGroup(int id)
+        {
+            return context.AssessmentResultGroups.Where(s => s.Id == id
+                                                             && s.IsActive)
+                .Include(s => 
+                    s.ResultTips.Where(a=>a.IsActive))
+                .First();
+        }
+
+        public List<ResultTip> GetTips(int groupId)
+        {
+            return context.ResultTips.Where(s => s.AssessmentResultGroupId == groupId
+                                                 && s.IsActive)
+                .Include(s=>s.AssessmentResultGroup)
+                .ToList();
+        }
     }
 }
